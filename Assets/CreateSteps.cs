dfd8a491e3f271f0a9967787b3de998ba27e0eb6
BzLOVEman <segareyarou@yahoo.co.jp>
2019-07-27 17:18:34 +0900

    start

---
+++ Assets/CreateSteps.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateSteps : MonoBehaviour {
+
+    private int stepSum;
+    public GameObject stepParent;
+
+    void Start() {
+        stepSum = 10;
+    }
+    
+    void Update() {
+
+    }
+
+    private void settings() {
+        for (int i = 0; i < stepSum; i++) {
+         //   GameObject stepTmp =GameObject()
+        }
+    }
+}
